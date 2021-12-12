6cf622f70652367a58b7cee17400d476fead0e6c
DevOps-T04 <aymen4real@gmail.com>
2021-12-12 01:45:59 +0100

    Base Template 🔥

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const PORT = 2015 || process.env.PORT
+
+
+app.get('/', (req, res)=>{
+    res.json({"message": "Hello Clarice 😬"})
+})
+
+
+app.get('*', (req, res)=>{
+    res.json({'message': '🙈 404!'})
+})
+app.listen(PORT, console.log(`✨ Live on http://localhost://${PORT}`))
