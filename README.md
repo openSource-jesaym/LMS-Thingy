be4b51222ec045daa19512f06e60164d8c1e6ccf
DevOps-T04 <91288221+MDS-ISALIB@users.noreply.github.com>
2021-12-16 14:40:06 +0100

    Auth0 🛡
    Authentication using Auth0 (Google)

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Route | Description
  / | Home route  
  / home | Feeds section  
  / home / subjects | list all subjects with their course count  
+ / login | Login route using Auth0  
+ / logout | Logout route using Auth0  
--- src/server.js
+++ src/server.js
@@ -1,9 +1,25 @@
 const express = require('express')
 const app = express()
 const PORT = 2015 || process.env.PORT
+require('dotenv').config()
+const { auth } = require('express-openid-connect')
+const { requiresAuth } = require('express-openid-connect')
 
+
+const config = {
+    authRequired: false,
+    auth0Logout: true,
+    secret: process.env.SECRET,
+    baseURL: process.env.BASEURL,
+    clientID: process.env.CLIENTID,
+    issuerBaseURL: process.env.ISSUERBASEURL
+}
+
+
+app.use(auth(config));
 app.get('/', (req, res)=>{
-    res.json({"message": "Hello Clarice 😬"})
+    // res.json({"message": "Hello Clarice 😬"})
+    res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
 })
 
 // ROUTES
