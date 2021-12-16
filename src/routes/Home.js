32921265507b7db6930c0d14a5fcc6d95578a786
DevOps-T04 <aymen4real@gmail.com>
2021-12-16 21:41:25 +0100

    Home/Index views 👓

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,7 +7,7 @@ const { getCoursesData, getSubjects} = require('../Functions/Home')
 router.get('/', (req, res)=>{
     getCoursesData()
     .then(courses=>{
-        res.json(courses)
+        res.render('home', { courses: courses})
     })
 })
 
