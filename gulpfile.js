8e2e2e753b297a091622540f331e5179b8b28080
DevOps-T04 <devops-t04@zmwarelab.com>
2022-01-15 19:50:00 +0100

    env variables setup 🗿

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,11 @@ gulp.task("build-clean", function () {
 });
 
 
+// Task which would just create a copy of the current views directory in dist directory
+gulp.task("env", function () {
+  return gulp.src("./src/config/env/.env").pipe(gulp.dest("./dist/config/env"));
+});
+
 // Task which would compile the sass files to css
 gulp.task("buildStyles", () => {
   return gulp
@@ -45,6 +50,7 @@ gulp.task(
     "build-clean",
     "typescript",
     "buildStyles",
+    "env",
     "views",
     "assets"
   ),
