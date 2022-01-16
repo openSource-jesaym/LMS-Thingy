b6282962fed58de8ab599005af9c4b8ef8096851
AymKh <aymen4real@gmail.com>
2022-01-16 22:22:19 +0100

    .env check 🎉
    Making sure the .env file always exists, so the build does not break.

--- runProduction.sh
+++ runProduction.sh
@@ -6,11 +6,31 @@
 cd /home/jelastic/ROOT
 # Installing all the necessary packages
 npm i
-# Renaming .env so it does not break the build
-mv src/config/env/.env\ _SAMPLE src/config/env/.env
+# Making sure the .env file exists
+ENVFILE=src/config/env/.env
+ENVSAMPLE=src/config/env/.envSAMPLE
+if test -f "$ENVFILE"; then
+    echo "🦄 $ENVFILE already exists, no fearthur steps needed"
+else
+    # .env doesn't exist, so check for .envSAMPLE instead
+	if test -f "$ENVSAMPLE"; then
+        echo "🦄 $ENVSAMPLE exists, it will be renamed to .env under src/config/env/.env."
+        # Renaming .env so it does not break the build
+        mv src/config/env/.envSAMPLE src/config/env/.env
+        echo "🦄 $ENVSAMPLE file has been renamed in /src"
+        sleep 2
+    else
+        # nor .env nor .envSAMPLE exist, so create a dummy .env
+        echo "🦄 .env file not found"
+        echo "🦄 $ENVSAMPLE not found, it will be created under src/config/env/.env"
+        touch src/config/env/.env
+	fi
+fi
 # making a fresh build
 npm run build
 # removing .env, prod env vars has already been setup
-rm src/config/env/.env
+sleep 5
+rm dis/config/env/.env
+echo "🦄 .env has been removed from dist/"
 # The server will be ran using PM2
 npm run prod
