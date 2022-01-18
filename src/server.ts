c9d4d06340caf8659dc682f96d91bb7bc2d900e7
AymKh <aymen4real@gmail.com>
2022-01-18 12:21:28 +0100

    home redirect

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname + '/public')));
 
 app.get("/", (req, res) => {
-  res.render("index");
+  res.redirect("/home");
 });
 
 // ROUTES
